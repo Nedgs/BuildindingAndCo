c2ef988d58fb4d85c70976449e195abd9d26c3e4
Nedgs <nadergwasmi25@gmail.com>
2021-12-16 14:00:16 +0100

    initialisation du projet

---
+++ src/main/java/com/buildindingandco/BuildindingAndCo/BuildindingAndCoApplication.java
@@ -0,0 +1,13 @@
+package com.buildindingandco.BuildindingAndCo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildindingAndCoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildindingAndCoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buildindingandco/BuildindingAndCo/BuildindingAndCoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildindingandco.BuildindingAndCo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildindingAndCoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
